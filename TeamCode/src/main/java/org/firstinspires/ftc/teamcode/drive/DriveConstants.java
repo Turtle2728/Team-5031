32e3814a8e8f6b99cc784a47763ad73159923210
turtle2728 <qzwxecrv0208@gmail.com>
2024-03-12 19:52:51 +0900

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0035 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.0022;
-    public static double kStatic = 0.0185;
+    public static double kV = 0.0175; // rpmToVelocity(MAX_RPM);
+    public static double kA = 0.0035;
+    public static double kStatic = 0.0022;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -65,8 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = 45;
+    public static double MAX_ACCEL = 45;
     public static double MAX_ANG_VEL = Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
