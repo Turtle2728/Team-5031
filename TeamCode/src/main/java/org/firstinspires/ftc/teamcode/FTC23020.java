53c8b1038c0373dbf3ffbfb4b2f530a1a2a74f58
Turtle2728 <qzwxecrv0208@gmail.com>
2024-03-22 21:45:36 +0900

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC23020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC23020.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
