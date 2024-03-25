33ef8259ec67eddb5cf6ddcb566cfe5bf2de4944
Turtle2728 <qzwxecrv0208@gmail.com>
2024-03-25 13:10:06 +0900

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC23020.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC23020.java
@@ -43,8 +43,8 @@ public class FTC23020 extends LinearOpMode {
 
         imu.initialize(parameters);
 
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
         ARM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         ARM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
